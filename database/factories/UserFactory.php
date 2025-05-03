5dd264631d162f38729dcfbd2b06c94a44f25ad9
stojdl <bedrich.stojdl@email.cz>
2025-05-03 20:46:41 +0200

    quick fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,6 @@ class UserFactory extends Factory
     {
         return [
             'username' => fake()->name(),
-            'hashtag' => fake()->numberBetween(1000, 9999),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
