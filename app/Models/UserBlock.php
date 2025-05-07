4ae33135397cc88416804489d26f3b2719653a64
stojdl <bedrich.stojdl@email.cz>
2025-05-07 14:38:33 +0200

    ♻️ refactor(user): rename user block entities
    - rename UserBlocksController to UserBlockController
    - rename UserBlocks model to UserBlock
    - rename create_user_blocks_table migration

--- app/Models/UserBlock.php
+++ app/Models/UserBlock.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class UserBlocks extends Model
+class UserBlock extends Model
 {
     use HasFactory, SoftDeletes;
 
