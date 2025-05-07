0be78c2d7c86d9688122e7854bee1adc7c20d5ad
stojdl <bedrich.stojdl@email.cz>
2025-05-07 14:27:23 +0200

    ✨ feat(factory): add friendship and user block factories
    - create FriendshipFactory to generate friendship data
    - create UserBlockFactory to generate user block data
    - create UserFollowFactory to generate user follow data

---
+++ database/factories/FriendshipFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Friendship>
+ */
+class FriendshipFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'follower_id' => \App\Models\User::factory(),
+            'followed_id' => \App\Models\User::factory(),
+            'action_user_id' => \App\Models\User::factory(),
+            'status' => $this->faker->randomElement(['pending', 'accepted', 'blocked']),
+        ];
+    }
+}
---
+++ database/factories/UserBlockFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class UserBlockFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'blocker_id' => \App\Models\User::factory(),
+            'blocked_id' => \App\Models\User::factory(),
+        ];
+    }
+}
