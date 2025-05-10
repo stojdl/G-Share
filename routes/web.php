4d342eac34decfedab5fccf63d7ba4fc05e6938c
stojdl <bedrich.stojdl@email.cz>
2025-05-10 21:12:31 +0200

    ♻️ refactor(auth): redirect users to share-place after login/registration
    - update redirection after login and registration to share-place
    - ensure consistent user experience after authentication

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/share-place', [PagesController::class, 'share_place'])->name('share_place');
+    Route::get('/share-place', [PagesController::class, 'share_place'])->name('share-place');
 
     Route::group(['prefix' => 'post'], function () {
         Route::post('/store', [PostController::class, 'store'])->name('post.store');
