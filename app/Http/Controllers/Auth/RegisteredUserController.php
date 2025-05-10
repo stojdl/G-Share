de216a35b508f34e7a3322b8e0ce84cba5230d39
stojdl <bedrich.stojdl@email.cz>
2025-05-10 21:13:04 +0200

    ???

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -47,6 +47,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('share-place', absolute: false));
+        return redirect(route('share_place', absolute: false));
     }
 }
