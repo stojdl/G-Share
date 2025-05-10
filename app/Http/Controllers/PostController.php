9cdc38a3f84abf45847e3392f220343732f8538e
stojdl <bedrich.stojdl@email.cz>
2025-05-10 19:14:04 +0200

    ✨ feat(post): implement post creation functionality
    - add store method to PostController to handle post creation
    - create PostRequest for validation
    - create Post model and define fillable attributes
    - add post store route in web.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function store(PostRequest $request)
+    {
+        $request->validated();
+
+        Post::create([
+            'user_id' => auth()->id(),
+            'title' => $request->input('title'),
+            'body' => $request->input('body'),
+        ]);
+
+        return back()->with('success', 'Post created successfully!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -17,11 +18,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/share-place', [PagesController::class, 'share_place'])->name('share_place');
+
+    Route::group(['prefix' => 'post'], function () {
+        Route::post('/store', [PostController::class, 'store'])->name('post.store');
+    });
+
 });
 
 // Routy pro další stránky
 Route::get('/dev', [PagesController::class, 'dev'])->name('dev');
-Route::get('/share-place', [PagesController::class, 'share_place'])->name('share_place'); // Dočasná routa
 
 // Routa pro zobrazení uživatelského profilu
 Route::get('/user/{user}', [PagesController::class, 'user'])->name('user');
