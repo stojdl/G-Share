6a1fd2682f213744d97ceaeb719bbe8c917f71c0
stojdl <bedrich.stojdl@email.cz>
2025-05-16 14:30:33 +0200

    ✨ feat(post): enhance post card with reactions and form
    - add post reaction form to allow users to react to posts
    - display reactions with counts, sorted by popularity
    - improve comment visibility with dynamic show/hide functionality

--- resources/js/Components/Cards/PostCard.tsx
+++ resources/js/Components/Cards/PostCard.tsx
@@ -3,6 +3,7 @@ import CommentCard from "./CommentCard";
 import H3 from "../Headings/H3";
 import CommentPostForm from "@/Fragments/Forms/CommentPostForm";
 import { Link } from "@inertiajs/react";
+import PostReactionForm from "@/Fragments/Forms/PostReactionForm";
 
 interface Props {
     post: any;
@@ -27,14 +28,26 @@ const PostCard = (props: Props) => {
                 <p className="text-gray-300">{post.body}</p>
                 <div className="flex items-center justify-between">
                     {post.reactions.length > 0 && (
-                        <p className="text-gray-400">
-                            {post.reactions?.map((reaction: any, k: number) => (
-                                <span key={k} className="text-red-500">
-                                    {reaction.reaction_type}
-                                    {k < post.reactions.length - 1 ? ", " : ""}
-                                </span>
-                            ))}
-                        </p>
+                        <div className="flex items-center space-x-2">
+                            {Object.entries(
+                                post.reactions.reduce(
+                                    (acc: any, reaction: any) => {
+                                        acc[reaction.reaction_type] =
+                                            (acc[reaction.reaction_type] || 0) +
+                                            1;
+                                        return acc;
+                                    },
+                                    {}
+                                )
+                            )
+                                .sort(([, a]: any, [, b]: any) => b - a)
+                                .map(([reaction, count]: any, k: number) => (
+                                    <span key={k} className="text-gray-400">
+                                        {reaction}
+                                        {count > 1 ? ` (${count})` : ""}
+                                    </span>
+                                ))}
+                        </div>
                     )}
                     <div className="flex items-center space-x-2">
                         {post.views.length > 0 && (
@@ -51,21 +64,25 @@ const PostCard = (props: Props) => {
                     </div>
                 </div>
             </div>
-
-            {post.comments?.length > 0 ? (
-                <button
-                    onClick={() => setShowComments(!showComments)}
-                    className="mt-4 font-semibold text-red-500 cursor-pointer hover:underline"
-                >
-                    💬 {showComments ? "Skrýt komentáře" : "Zobrazit komentáře"}
-                    {post.comments?.length > 0
-                        ? ` (${post.comments?.length})`
-                        : ` (${post.comments?.length})`}
-                </button>
-            ) : (
-                <p>Zatím žádné komentáře.</p>
-            )}
-
+            <div className="mt-4 flex justify-between">
+                <PostReactionForm />
+                {post.comments?.length > 0 ? (
+                    <button
+                        onClick={() => setShowComments(!showComments)}
+                        className="font-semibold text-red-500 cursor-pointer hover:underline"
+                    >
+                        💬{" "}
+                        {showComments
+                            ? "Skrýt komentáře"
+                            : "Zobrazit komentáře"}
+                        {post.comments?.length > 0
+                            ? ` (${post.comments?.length})`
+                            : ` (${post.comments?.length})`}
+                    </button>
+                ) : (
+                    <p>Zatím žádné komentáře.</p>
+                )}
+            </div>
             {showComments && (
                 <div className="mt-2 space-y-2">
                     {post.comments?.map(
